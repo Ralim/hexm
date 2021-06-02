21b0ddb607fc88ca2e7bb5acb8be5a57ea7b566f
Ben V. Brown <ralim@ralimtek.com>
2021-06-02 18:12:09 +1000

    split segment ops

--- hexm.go
+++ hexm.go
@@ -24,68 +24,13 @@ func main() {
 	outputMemory := gohex.NewMemory()
 	//Parse all input files into virtual memory space
 	for i, inputFilePath := range inputFiles {
-		mem := gohex.NewMemory()
-		{
-			fmt.Printf("Loading file %d\r\n", i+1)
-			file, err := os.Open(inputFilePath)
-			if err != nil {
-				panic(err)
-			}
-			defer file.Close()
-			err = mem.ParseIntelHex(file)
-			if err != nil {
-				panic(err)
-			}
+		fmt.Printf("Loading file %d => %s\r\n", i+1, inputFilePath)
+		mem, err := parseInputFile(inputFilePath)
+		if err != nil {
+			panic(err)
 		}
-		existingSegments := outputMemory.GetDataSegments()
-
-		for x, segment := range mem.GetDataSegments() {
-			fmt.Printf("Section %d @ 0x%08X ; len %d\n", x+1, segment.Address, len(segment.Data))
-			//Check if this segment overlaps the existing segments
-			if len(existingSegments) > 0 {
-				for _, seg2 := range existingSegments {
-					if !segmentOverlaps(segment, seg2) || userConfirmOverlap(segment, inputFilePath) {
-						//write this segment into it
-						outputMemory.SetBinary(segment.Address, segment.Data)
-					}
-				}
-			} else {
-				outputMemory.SetBinary(segment.Address, segment.Data)
-			}
-		}
+		mergeSegments(outputMemory, mem, inputFilePath)
 	}
 	// Now we want to write out the file, if its hex then we can use the hex writer, otherwise we will want to persist it out to bin
-	outputHex, binaryStart, err := parseFileTypeAndStart(outputFile)
-	if err != nil {
-		panic(err)
-	}
-	file, err := os.Create(outputFile)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	if outputHex {
-		outputMemory.DumpIntelHex(file, 32)
-	} else {
-		//We want to write a binary file starting at the specified location, and padding all gaps
-		existingSegments := outputMemory.GetDataSegments()
-		//Write out each section
-		for i, section := range existingSegments {
-			start := section.Address - uint32(binaryStart)
-			fmt.Printf("Writing %v bytes @ %08X for section %d\r\n", len(section.Data), start, i+1)
-			_, err = file.WriteAt(section.Data, int64(start))
-			if err != nil {
-				panic(err)
-			}
-		}
-	}
-}
-
-func segmentOverlaps(seg gohex.DataSegment, seg2 gohex.DataSegment) bool {
-	if ((seg2.Address >= seg.Address) && (seg2.Address < seg.Address+uint32(len(seg.Data)))) ||
-		((seg2.Address < seg.Address) && (seg2.Address+uint32(len(seg2.Data))) > seg.Address) {
-		return true
-	}
-	return false
+	writeOutput(outputFile, outputMemory)
 }
---
+++ segment.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/marcinbor85/gohex"
+)
+
+func parseInputFile(path string) (*gohex.Memory, error) {
+	mem := gohex.NewMemory()
+
+	file, err := os.Open(path)
+	if err != nil {
+		return mem, err
+	}
+	defer file.Close()
+	err = mem.ParseIntelHex(file)
+	if err != nil {
+		return mem, err
+
+	}
+	return mem, nil
+}
+
+func mergeSegments(base, addional *gohex.Memory, userPath string) {
+	existingSegments := base.GetDataSegments()
+	for x, segment := range addional.GetDataSegments() {
+		fmt.Printf("Section %d @ 0x%08X ; len %d\n", x+1, segment.Address, len(segment.Data))
+		//Check if this segment overlaps the existing segments
+		if len(existingSegments) > 0 {
+			for _, seg2 := range existingSegments {
+				if !segmentOverlaps(segment, seg2) || userConfirmOverlap(segment, userPath) {
+					//write this segment into it
+					base.SetBinary(segment.Address, segment.Data)
+				} else {
+					fmt.Printf("Did not merge the segment @ %08X", segment.Address)
+				}
+			}
+		} else {
+			base.SetBinary(segment.Address, segment.Data)
+		}
+	}
+}
+
+func writeOutput(outputFile string, outputMemory *gohex.Memory) {
+	outputHex, binaryStart, err := parseFileTypeAndStart(outputFile)
+	if err != nil {
+		panic(err)
+	}
+	file, err := os.Create(outputFile)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	if outputHex {
+		outputMemory.DumpIntelHex(file, 32)
+	} else {
+		//We want to write a binary file starting at the specified location, and padding all gaps
+		existingSegments := outputMemory.GetDataSegments()
+		//Write out each section
+		for i, section := range existingSegments {
+			start := section.Address - uint32(binaryStart)
+			fmt.Printf("Writing %v bytes @ %08X for section %d\r\n", len(section.Data), start, i+1)
+			_, err = file.WriteAt(section.Data, int64(start))
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
+
+func segmentOverlaps(seg gohex.DataSegment, seg2 gohex.DataSegment) bool {
+	if ((seg2.Address >= seg.Address) && (seg2.Address < seg.Address+uint32(len(seg.Data)))) ||
+		((seg2.Address < seg.Address) && (seg2.Address+uint32(len(seg2.Data))) > seg.Address) {
+		return true
+	}
+	return false
+}
